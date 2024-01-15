e4038bbf3a0142b29efca7d36af9d7d7b75b50b3
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-15 16:33:08 +0100

    Completed header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,10 +14,41 @@ export default {
     <header>
         <div class="container">
             <div class="row">
-                <input type="text" placeholder="Cerca il tuo film preferito" class="form-control my-3" v-model="store.query" @keyup.enter="$emit('card_search')">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h1 class="text-uppercase">Boolflix</h1>
+                    <div class="d-flex align-items-center">
+                        <input type="text" placeholder="Cerca il tuo film preferito" class="form-control my-3" v-model="store.query" @keyup.enter="$emit('card_search')">
+                        <button>Cerca</button>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    background-color: #000000;
+    padding: 10px;
+
+    h1 {
+        color: #ff180a;
+    }
+
+    input {
+        width: 400px;
+    }
+
+    button {
+        color: white;
+        background-color: #ff180a;
+        padding: 10px;
+        border-style: none;
+        border-radius: 10px;
+
+        &:hover {
+            background-color: #c9150b;
+        }
+    }
+}
+</style>
